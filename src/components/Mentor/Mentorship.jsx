5c6f037b4e8bdf3443a4db59ad110196ff8d6184
Raghav Jindal <raghavjindal0212@gmail.com>
2023-08-06 19:51:04 +0530

    updated index.html

--- src/components/Mentor/Mentorship.jsx
+++ src/components/Mentor/Mentorship.jsx
@@ -18,6 +18,9 @@ import instructor1 from "../../assets/instructor1.jpg";
 import instructor2 from "../../assets/instructor2.jpg";
 import { motion } from "framer-motion";
 import { slideAnimateVariants } from "../animate/animate";
+import inst3 from "../../assets/inst3.png";
+import inst4 from "../../assets/inst4.png";
+import inst5 from "../../assets/inst5.jpg";
 
 export default function Mentorship() {
   const [noOfCourses, setnoOfCourses] = useState(0);
@@ -27,9 +30,17 @@ export default function Mentorship() {
   return (
     <div className="w-full flex-col h-full flex  items-center align-middle">
       <img src={meeting1} style={{ position: "absolute", left: "0", backgroundBlendMode: "lighten", opacity: "0.5" }} />
-      <motion.p style={{ fontSize: "25px", zIndex: "50" }} className="text-xl font-mono mt-12 p-5 mx-3 mentor-p" variants={slideAnimateVariants} initial="initial" whileInView="animate"  viewport={{
-                  once: true,
-                }} custom={1}>
+      <motion.p
+        style={{ fontSize: "25px", zIndex: "50" }}
+        className="text-xl font-mono mt-12 p-5 mx-3 mentor-p"
+        variants={slideAnimateVariants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{
+          once: true,
+        }}
+        custom={1}
+      >
         Meet your Instructors/Mentors and join group sessions powered by{" "}
         <a href="https://huddle01.com/" target="_blank" style={{ textDecoration: "underline" }}>
           Huddle01 🚀
@@ -54,7 +65,7 @@ export default function Mentorship() {
           }
           portfolio={"/"}
           img={instructor1}
-          value = {3}
+          value={3}
         />
         <MentorCard
           title={"Web/App Developer"}
@@ -65,6 +76,42 @@ export default function Mentorship() {
           portfolio={"jaskirat.me"}
           info={
             "Meet Jaskirat Singh, your blockchain educator and guide into the fascinating world of decentralized technologies! With a deep passion for blockchain, I have dedicated 1.5 years to mastering this groundbreaking technology. As a seasoned developer and educator, I simplify complex concepts, making them accessible to learners of all backgrounds. Join me in the Blockchain Basics course and embark on a transformative experience, understanding the foundational principles, grasping fundamental building blocks, and applying this knowledge to shape a decentralized future—one block at a time!"
+          }
+          value={5}
+        />
+        <MentorCard
+          title={"Video Editor"}
+          name={"Manas Pathak"}
+          img={inst3}
+          session_name={"AMA session"}
+          date={"15 August 2023 - 2pm IST"}
+          portfolio={"/"}
+          info={
+            "Hey, I'm Manas Pathak, a video editing maestro with 1.5 years of experience. I love crafting cinematic masterpieces that captivate audiences. Join me on this exciting journey through the world of video editing, where we'll unlock the full potential of visual storytelling together! Let's get started and create magic with our videos!"
+          }
+          value={3}
+        />
+        <MentorCard
+          title={"Software Developer"}
+          name={"Harsh"}
+          img={inst4}
+          session_name={"Live class"}
+          date={"18 August 2023 - 1pm IST"}
+          portfolio={"/"}
+          info={
+            "A dynamic and enthusiastic individual with a proven track record of delivering high quality content around Data Science, Machine Learning, Deep Learning, Web 3.0 and Programming in general. "
+          }
+          value={5}
+        />
+        <MentorCard
+          title={"UI/UX Designer"}
+          name={"Preet Patel"}
+          img={inst5}
+          session_name={"Hands-on class"}
+          date={"11 August 2023 - 1pm IST"}
+          portfolio={"/"}
+          info={
+            "Hey, I am Preet Patel. I am UI/UX designer. UI/UX designer is a budding creative force in the dynamic realm of user interface and user experience design. Armed with a fresh perspective and a natural affinity for technology, this young designer possesses a unique ability to bridge the gap between contemporary design trends and the preferences of their generation."
           }
           value={5}
         />
