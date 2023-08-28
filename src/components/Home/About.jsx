85e02a6cfdae362a56fc7e16325a2f0e20eb1b1f
SrikarMK <srikarmks@gmail.com>
2023-08-29 00:28:59 +0530

    Bug fixes

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -3,10 +3,10 @@ import aboutImg from "../../assets/about.svg"
 export default function About() {
   return (
     <div className="" id="about">
-      <div className="px-32 py-20 flex flex-col items-center gap-16">
+      <div className="px-10 md:px-32 py-20 flex flex-col items-center gap-16">
         <h1 className="font-extrabold text-3xl">About Us</h1>
         <div className="flex items-start gap-32">
-          <p className="md:w-[70%] flex-col text-l">
+          <p className="md:w-[70%] flex-col md:text-l">
           <span className="font-bold">Mission:</span> Redefining education through decentralization, turning learning into a valuable asset. <br /><br></br>
 
           <span className="font-bold">Courses:</span> Explore Web2 and Web3 subjects for cutting-edge knowledge. <br /><br />
