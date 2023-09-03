35ce0747fc403d30053a6847f430da111f564a9c
Raghav Jindal <raghavjindal0212@gmail.com>
2023-09-03 19:33:14 +0530

    changed client id

--- src/RainbowWeb3authConnector.jsx
+++ src/RainbowWeb3authConnector.jsx
@@ -14,7 +14,8 @@ export const rainbowWeb3AuthConnector = ({ chains }) => {
   // Create Web3Auth Instance
 
   // const clientId = "BMkKHE4n2KgzLWFXDmpCVIpWMggQ8Pe8_4pRkbm9aNafKnn0WRlb1zoy6JlOh2nN2Aw54jIAbFbsAUut3tuJr8w";
-  const clientId = "BEiKywx7OIUl832MkrJekbhIGEq78pgkdLOYFhAz0aamsGLXPqInKea2MV33-J3AkoSRJ46_7Tm48yZwug7QsH8";
+  // const clientId = "BEiKywx7OIUl832MkrJekbhIGEq78pgkdLOYFhAz0aamsGLXPqInKea2MV33-J3AkoSRJ46_7Tm48yZwug7QsH8";
+  const clientId = "BCUIf4lI2Zbrd_xE-ZKigU9ptoF61GrzRuZJ_QBCNiAHqFtI4DXT62xD1lHW1e4kxqwVs_FpxZxy-e_FfzoU0eY";
   const chainConfig = {
     chainNamespace: CHAIN_NAMESPACES.EIP155,
     chainId: "0x" + chains[0].id.toString(16),
