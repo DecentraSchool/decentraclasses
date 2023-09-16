8012f3ed8e841ab8c5f7623be514610014767b51
Amruth Kiran <amruthkiran47@gmail.com>
2023-09-16 15:09:37 +0530

    animated nav icon

---
+++ src/components/Home/Burger.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Burger = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
+  
+    return (
+      <button
+        className="flex flex-col h-10 w-10 justify-center items-center group"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen
+              ? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
+              : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen
+              ? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
+              : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+      </button>
+    );
+};
+
+export default Burger;
