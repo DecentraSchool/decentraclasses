afaeb934fa3d876623b32d94c64e3b5c41f24070
Raghav Jindal <raghavjindal0212@gmail.com>
2024-01-29 15:57:16 +0530

    .

---
+++ src/constants/ICPcontent.js
@@ -0,0 +1,69 @@
+export const ICPcontent = {
+  section: [
+    {
+      title: "Section 1",
+      lecturesCount: "1",
+      totalDuration: "1 Hour",
+      lecture: [
+        {
+          title: "ICP Developer Journey 0.1",
+          desc: "The ICP Developer Journey is designed to give both new and existing developers a clear learning path to develop on the Internet Computer. Starting at level 0 with an introduction to the Internet Computer, terminology, and setting up a developer environment, to level 5 that details building complex dapps, the Developer Journey contains essential information for developers of all skill levels.",
+          videoUrl: "https://youtu.be/oBUpJ4CqmN0?si=OrKQp6pv-pASXb_I",
+          usersCompleted: [],
+        },
+      ],
+    },
+    {
+      title: "Section 2",
+      lecturesCount: "1",
+      totalDuration: "1 Hour",
+      lecture: [
+        {
+          title: "ICP Developer Journey 0.2",
+          desc: "In this episode of the ICP Developer Journey, developers are introduced to some of the most commonly used terminology that they should be aware of when building on the Internet Computer.",
+          videoUrl: "https://youtu.be/3WpP8ux1zX0?si=FO-q0TQbybKhQVKd",
+          usersCompleted: [],
+        },
+      ],
+    },
+    {
+      title: "Section 3",
+      lecturesCount: "1",
+      totalDuration: "1 Hour",
+      lecture: [
+        {
+          title: "ICP Developer Journey 0.3",
+          desc: "In this installment of the ICP Developer Journey, developers delve into the process of configuring their local development environment. This includes getting acquainted with a command line interface, installing the IC SDK, and establishing a new directory specifically for their developer journey projects.",
+          videoUrl: "https://youtu.be/fDMHUdo7m-k?si=9GNMXybzbjh98v9n",
+          usersCompleted: [],
+        },
+      ],
+    },
+    {
+      title: "Section 4",
+      lecturesCount: "1",
+      totalDuration: "1 Hour",
+      lecture: [
+        {
+          title: "ICP Developer Journey 0.4",
+          desc: "In this episode of the ICP Developer Journey, developers are introduced to canisters, their architecture, and will learn about the different types of canisters that can be developed. ",
+          videoUrl: "https://youtu.be/mwbRRk9T5Nw?si=8WJBNfUh0KreEDnQ",
+          usersCompleted: [],
+        },
+      ],
+    },
+    {
+      title: "Section 5",
+      lecturesCount: "1",
+      totalDuration: "1 Hour",
+      lecture: [
+        {
+          title: "ICP Developer Journey 0.5",
+          desc: "In this episode of the ICP Developer Journey, developers will learn about the different languages that can be used to develop dapps, and be provided a base-level introduction to the IC's two primarily supported languages, Motoko and Rust.",
+          videoUrl: "https://youtu.be/sZxbRAwYYMw?si=419U75O4l1YffTio",
+          usersCompleted: [],
+        },
+      ],
+    },
+  ],
+};
