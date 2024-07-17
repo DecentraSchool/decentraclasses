41b959bfc2cabbbc9165fe268b1eba17f6fa4ad7
Raghav Jindal <raghavjindal0212@gmail.com>
2024-07-17 16:15:23 +0530

    ..

--- src/components/Home/Recogonised.jsx
+++ src/components/Home/Recogonised.jsx
@@ -36,6 +36,33 @@ export default function Recogonised() {
                 <div className="flex items-center gap-4 w-[180px] px-4 py-3 rounded-md backdrop-blur-sm  ">
                     <img src={up} className="" alt="" />
                 </div>
+
+                <div className="flex items-center gap-4 w-[200px]  rounded-md backdrop-blur-sm  ">
+                    <img className="w-full h-full" src="https://www.startupindia.gov.in/content/dam/invest-india/newhomepage/Logo1.png" />
+                </div>
+                <div className="flex items-center gap-4 w-[180px] px-4 py-3 rounded-md backdrop-blur-sm  ">
+                    <img src="https://startinup.up.gov.in/wp-content/themes/blankslate/assets/img/logo2.png" className="" alt="" />
+                </div>
+                <div className="flex items-center gap-4 w-[220px] px-4 py-3 rounded-md backdrop-blur-sm  ">
+                    <img src={hub} className="w-full" alt="" />
+                    {/* <h3>Hub</h3> */}
+                </div>
+                <div className="flex items-center gap-4 w-[180px] px-4 py-3 rounded-md backdrop-blur-sm  ">
+                    <img src="https://www.glbitm.org/Uploads/image/me-LOGO-GLBCRI-july21.jpg" className="" alt="" />
+                    <h3>GL Bajaj Incubation</h3>
+                </div>
+                <div className="flex items-center gap-4 w-[220px] px-4 py-3 rounded-md backdrop-blur-sm  ">
+                    <img src={aws} className="full" alt="" />
+                </div>
+                <div className="flex items-center gap-4 w-[280px] px-4 py-3 rounded-md backdrop-blur-sm  ">
+                    <img src={corner} className="w-full" alt="" />
+                </div>
+                <div className="flex items-center gap-4 w-[220px] px-4 py-3 rounded-md backdrop-blur-sm  ">
+                    <img src={innovation} className="w-full" alt="" />
+                </div>
+                <div className="flex items-center gap-4 w-[180px] px-4 py-3 rounded-md backdrop-blur-sm  ">
+                    <img src={up} className="" alt="" />
+                </div>
             </div>
         </div>
     );
